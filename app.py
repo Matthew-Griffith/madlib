1a82bc71df0d54b3ea7cc63b54ac020497cc8b78
Matthew Griffith <matthew-griffith@protonmail.com>
2018-11-21 22:31:39 -0500

    I got the user content from the form on the index page to transfer over the /submitted page

--- app.py
+++ app.py
@@ -5,3 +5,12 @@ app = flask.Flask(__name__)
 @app.route('/')
 def index():
     return flask.render_template('index.html')
+
+@app.route('/submitted', methods=['POST', 'GET'])
+def submitted():
+    if flask.request.method == 'POST':
+        user_content = flask.request.form['user_content']
+        return flask.render_template('madlib_form.html', user_content=user_content)
+        # return str(flask.request.form['user-content'])
+    else:
+        return 'this was a get'
