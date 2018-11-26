36db881d922b83eab923388f17b1d3d89c60f4f2
Matthew Griffith <matthew-griffith@protonmail.com>
2018-11-25 21:55:59 -0500

    moved the tagging of the text into its own function

--- app.py
+++ app.py
@@ -9,15 +9,15 @@ def index():
 
 @app.route('/submitted', methods=['POST', 'GET'])
 def submitted():
-    if flask.request.method == 'POST':        
+    if flask.request.method == 'POST':             
         
-        st = StanfordPOSTagger('english-left3words-distsim.tagger',
-            path_to_jar='stanford-postagger-3.9.2.jar')
-        
-        user_text = flask.request.form['user_content'].replace('\n', ' -: ')
-        text_tagged = st.tag(user_text.split(' '))     
-        
-        return flask.render_template('madlib_form.html', user_content=text_tagged)
+        return flask.render_template('madlib_form.html', 
+            user_content=tag_text(flask.request.form['user_content']))
         # return str(flask.request.form['user_content'].split('\n'))
     else:
         return 'this was a get'
+
+def tag_text(text):
+    st = StanfordPOSTagger('english-left3words-distsim.tagger',
+        path_to_jar='stanford-postagger-3.9.2.jar')
+    return st.tag(text.replace('\n', ' -: ').split(' '))
